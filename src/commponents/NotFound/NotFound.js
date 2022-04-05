1afa63d9bc80390efc45186dad6d3dc29844a4c8
Amortha <amitsharma1998k@gmail.com>
2022-04-05 07:37:47 +0600

    NotFondComplit

--- src/commponents/NotFound/NotFound.js
+++ src/commponents/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import './NotFond.css'
 const NotFound = () => {
     return (
         <div>
-            <h1>Not found</h1>
+            <div className='not-found'>
+            <h1>Opps! Not Found! <br /> 404</h1>
+        </div>
         </div>
     );
 };
